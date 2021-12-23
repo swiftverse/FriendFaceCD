761e6a2e351f2208f781b955340c72ab026c4b36
shivgirja <amitsrivastava115@yahoo.com>
2021-12-23 14:07:50 +0530

    Initial Commit

---
+++ FriendFaceCD/FriendFaceCDApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceCDApp.swift
+//  FriendFaceCD
+//
+//  Created by Amit Shrivastava on 23/12/21.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceCDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
